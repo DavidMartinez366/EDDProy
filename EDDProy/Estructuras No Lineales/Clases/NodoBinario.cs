f31f86a1868d7c890fda861a1f6bbef740c6c1c1
david martinez <al23760329@ite.edu.mx>
2024-10-18 09:45:06 -0700

    se agrego constructor a la clase nodo

--- EDDProy/Estructuras No Lineales/Clases/NodoBinario.cs
+++ EDDProy/Estructuras No Lineales/Clases/NodoBinario.cs
@@ -11,5 +11,15 @@ namespace EDDemo.Estructuras_No_Lineales
         public int Dato;
         public NodoBinario Izq;
         public NodoBinario Der;
+
+        public NodoBinario(int dato)
+        {
+            this.Dato = Dato;
+            this.Izq = null;
+            this.Der = null;
+
+        }
+
     }
+    
 }
